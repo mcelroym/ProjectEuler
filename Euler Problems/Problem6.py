4f4af0e74b7c01df31a2eb9b8a38d0ed4d76dc5f
mcelroym <masonic@gmail.com>
2017-08-25 13:09:24 -0700

    Add files via upload

--- Euler Problems/Problem6.py
+++ Euler Problems/Problem6.py
@@ -1,6 +1,3 @@
-# 1 + 2 + 3 +.... + n
-# n * (n+1) / 2
-
 def sum_of_squares(n):
     sum = n * (n+1) * (2*n + 1) / 6
     return sum
--- Euler Problems/UserProblem1.py
+++ Euler Problems/UserProblem1.py
@@ -1,16 +1,13 @@
 import math
 
 class Square:
-    center = 0
-    vertex1 = 0
-    vertex2 = 0
-    vertex3 = 0
-    vertex4 = 0
-    area = 0
+    center = [0, 0]
+    vertex = [[0,0], [0,0], [0,0], [0,0]]
+    area = [0, 0]
 
     def __init__(self, center, vertex):
         self.area = 2 * self.EuclideanDistance(center, vertex) ** 2
-        self.vertex1 = vertex
+        self.vertex[0] = vertex
 
         #find vertex2, vertex3, vertex4
 
@@ -18,12 +15,11 @@ class Square:
         return math.sqrt((x[0]-y[0])**2 + (x[1]-y[1])**2)
 
     def OverlappingArea(self, square):
+        # Use Shoestring Algorithm
         pass
 
     def Intersections(self, square):
         pass
-
-
 
 test = Square([0,0],[1,1])
 print test.area
