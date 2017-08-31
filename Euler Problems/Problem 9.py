04b3d9b4d3585f7b6a0bde9fc47d798a2b7d4b8d
Mason McElroy <masonic@gmail.com>
2017-08-30 22:31:38 -0700

    Delete Problem 9.py

--- Euler Problems/Problem 9.py
+++
@@ -1,8 +0,0 @@
-
-for a in range(0, 1000):
-    for b in range(a+1, 1000):
-        for c in range(b+1, 1000):
-            if a + b + c != 1000:
-                continue
-            if a**2 + b**2 == c**2:
-                print a * b * c
